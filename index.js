f2d1edbf7bb4932a8c2050eb5b8ee6c113a9e9d0
Vitória Bastos <vitoriabastosb@gmail.com>
2025-05-02 17:39:11 -0300

    Teste workflow2

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ const sendFiles = require("./routes/send-files");
 app.use("/send-files", sendFiles);
 
 // Inicia o servidor na porta 6005
-app.listen(80, "0.0.0.0", () => {
+app.listen(6005, "0.0.0.0", () => {
   console.log("Server on port 6005");
 });
